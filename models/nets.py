55222048f7238e5d46a0e931bce044f1e940ea84
pouyatoroghi <145000495+pouyatoroghi@users.noreply.github.com>
2025-07-26 17:39:33 +0330

    Update nets.py

--- models/nets.py
+++ models/nets.py
@@ -1,5 +1,142 @@
-import torch
+# import torch
 
+# from torch.nn import Module, Sequential, Linear, Tanh, Parameter, Embedding
+# from torch.distributions import Categorical, MultivariateNormal
+
+# if torch.cuda.is_available():
+#     from torch.cuda import FloatTensor
+#     torch.set_default_tensor_type(torch.cuda.FloatTensor)
+# else:
+#     from torch import FloatTensor
+
+
+# class PolicyNetwork(Module):
+#     def __init__(self, state_dim, action_dim, discrete) -> None:
+#         super().__init__()
+
+#         self.net = Sequential(
+#             Linear(state_dim, 50),
+#             Tanh(),
+#             Linear(50, 50),
+#             Tanh(),
+#             Linear(50, 50),
+#             Tanh(),
+#             Linear(50, action_dim),
+#         )
+
+#         self.state_dim = state_dim
+#         self.action_dim = action_dim
+#         self.discrete = discrete
+
+#         if not self.discrete:
+#             self.log_std = Parameter(torch.zeros(action_dim))
+
+#     def forward(self, states):
+#         if self.discrete:
+#             probs = torch.softmax(self.net(states), dim=-1)
+#             distb = Categorical(probs)
+#         else:
+#             mean = self.net(states)
+
+#             std = torch.exp(self.log_std)
+#             cov_mtx = torch.eye(self.action_dim) * (std ** 2)
+
+#             distb = MultivariateNormal(mean, cov_mtx)
+
+#         return distb
+
+
+# class ValueNetwork(Module):
+#     def __init__(self, state_dim) -> None:
+#         super().__init__()
+
+#         self.net = Sequential(
+#             Linear(state_dim, 50),
+#             Tanh(),
+#             Linear(50, 50),
+#             Tanh(),
+#             Linear(50, 50),
+#             Tanh(),
+#             Linear(50, 1),
+#         )
+
+#     def forward(self, states):
+#         return self.net(states)
+
+
+# class Discriminator(Module):
+#     def __init__(self, state_dim, action_dim, discrete) -> None:
+#         super().__init__()
+
+#         self.state_dim = state_dim
+#         self.action_dim = action_dim
+#         self.discrete = discrete
+
+#         if self.discrete:
+#             self.act_emb = Embedding(
+#                 action_dim, state_dim
+#             )
+#             self.net_in_dim = 2 * state_dim
+#         else:
+#             self.net_in_dim = state_dim + action_dim
+
+#         self.net = Sequential(
+#             Linear(self.net_in_dim, 50),
+#             Tanh(),
+#             Linear(50, 50),
+#             Tanh(),
+#             Linear(50, 50),
+#             Tanh(),
+#             Linear(50, 1),
+#         )
+
+#     def forward(self, states, actions):
+#         return torch.sigmoid(self.get_logits(states, actions))
+
+#     def get_logits(self, states, actions):
+#         if self.discrete:
+#             actions = self.act_emb(actions.long())
+
+#         sa = torch.cat([states, actions], dim=-1)
+
+#         return self.net(sa)
+
+
+# class Expert(Module):
+#     def __init__(
+#         self,
+#         state_dim,
+#         action_dim,
+#         discrete,
+#         train_config=None
+#     ) -> None:
+#         super().__init__()
+
+#         self.state_dim = state_dim
+#         self.action_dim = action_dim
+#         self.discrete = discrete
+#         self.train_config = train_config
+
+#         self.pi = PolicyNetwork(self.state_dim, self.action_dim, self.discrete)
+
+#     def get_networks(self):
+#         return [self.pi]
+
+#     def act(self, state):
+#         self.pi.eval()
+
+#         print(state.shape)
+
+#         state = FloatTensor(state)
+#         distb = self.pi(state)
+
+#         action = distb.sample().detach().cpu().numpy()
+
+#         return action
+
+
+
+import torch
 from torch.nn import Module, Sequential, Linear, Tanh, Parameter, Embedding
 from torch.distributions import Categorical, MultivariateNormal
 
@@ -33,17 +170,14 @@ class PolicyNetwork(Module):
 
     def forward(self, states):
         if self.discrete:
-            probs = torch.softmax(self.net(states), dim=-1)
-            distb = Categorical(probs)
+            logits = self.net(states)
+            probs = torch.softmax(logits, dim=-1)
+            return Categorical(probs)
         else:
             mean = self.net(states)
-
             std = torch.exp(self.log_std)
-            cov_mtx = torch.eye(self.action_dim) * (std ** 2)
-
-            distb = MultivariateNormal(mean, cov_mtx)
-
-        return distb
+            cov_mtx = torch.diag_embed(std ** 2)  # Supports batch input
+            return MultivariateNormal(mean, cov_mtx)
 
 
 class ValueNetwork(Module):
@@ -73,9 +207,7 @@ class Discriminator(Module):
         self.discrete = discrete
 
         if self.discrete:
-            self.act_emb = Embedding(
-                action_dim, state_dim
-            )
+            self.act_emb = Embedding(action_dim, state_dim)
             self.net_in_dim = 2 * state_dim
         else:
             self.net_in_dim = state_dim + action_dim
@@ -98,18 +230,11 @@ class Discriminator(Module):
             actions = self.act_emb(actions.long())
 
         sa = torch.cat([states, actions], dim=-1)
-
         return self.net(sa)
 
 
 class Expert(Module):
-    def __init__(
-        self,
-        state_dim,
-        action_dim,
-        discrete,
-        train_config=None
-    ) -> None:
+    def __init__(self, state_dim, action_dim, discrete, train_config=None) -> None:
         super().__init__()
 
         self.state_dim = state_dim
@@ -125,11 +250,11 @@ class Expert(Module):
     def act(self, state):
         self.pi.eval()
 
-        print(state.shape)
-
-        state = FloatTensor(state)
+        # Ensure state has batch dimension
+        state = FloatTensor(state).unsqueeze(0)  # (1, state_dim)
         distb = self.pi(state)
 
         action = distb.sample().detach().cpu().numpy()
 
-        return action
+        # Return scalar for discrete action, otherwise full action vector
+        return action[0] if self.discrete else action[0, :]
